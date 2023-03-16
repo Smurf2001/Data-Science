b4cdc375603f9fa68fabffce357132d99cedbfc7
Smurf2001 <125333647+Smurf2001@users.noreply.github.com>
2023-03-15 20:46:12 -0500

    Delete capstone_4950 directory

--- capstone_4950/capstone_4950/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Top-level package for Capstone 4950."""
-
-__author__ = """Smruti Patel"""
-__email__ = 'smrutii.patell@gmail.com'
-__version__ = '0.1.0'
--- capstone_4950/docs/usage.rst
+++
@@ -1,7 +0,0 @@
-=====
-Usage
-=====
-
-To use Capstone 4950 in a project::
-
-    import capstone_4950
